bdaad32cf5447eb1ad4ae5320a4f4a5985919308
Kenton Cooley <kenton@darkwolf.ventures>
2021-08-11 13:22:19 -0500

    Delivers invalidate error on 200 HTTP response with invalid json

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -64,6 +64,18 @@ class RemoteFeedLoaderTests: XCTestCase {
             XCTAssertEqual(capturedErrors, [.invalidData])
         }
         
+        func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON(){
+            let (sut, client) = makeSUT()
+            
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load { capturedErrors.append($0)}
+            
+            let invalidJSON = Data("invalid json".utf8)
+            client.complete(withStatusCode: 200, data: invalidJSON)
+            
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
+        
     }
     
     //MARK: - helpers
@@ -91,9 +103,9 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, index: Int = 0){
+        func complete(withStatusCode code: Int, data: Data = Data(), index: Int = 0){
             let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
         
     }
